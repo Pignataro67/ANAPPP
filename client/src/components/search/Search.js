480f9bde40fea839f2183a1ad1af85d3f715f710
Pignataro67 <Pignataro67@gmail.com>
2020-06-05 04:56:13 -0700

    adding state

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
 import SearchInput from "./SearchInput";
+import Button from '../Button';
+import Card from '../Card';
 
 class Search extends Component {
+  state = {
+    startingLocation: '',
+    destination: ''
+}
+
+handleOnSubmit = (e) => {
+  e.preventDefault()
+  console.log(this.state)
+}
+
+handleChangeStart = ({value: startingLocation}) => {
+  this.setState({
+    startingLocation
+  })
+}
+
+handleChangeDestination = ({value: destination}) => {
+  this.setState({
+    destination
+  })
+}
   render() {
     return (
       <form>
