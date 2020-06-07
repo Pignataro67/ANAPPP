72095637e7fc4b3c7389c73e79fd252897df723d
Pignataro67 <Pignataro67@gmail.com>
2020-06-07 07:01:17 -0700

    adding action

--- client/src/actions/fetchLocations.js
+++ client/src/actions/fetchLocations.js
@@ -1,3 +1,15 @@
+function _fetchLocation(input) {
+  debugger
+  return fetch(`/search_start/${input}`)
+  .then(response => response.json())
+  .then(locations => locations.predictions)
+}
+
 export function fetchStartingLocation(input) {
-  console.log(input);
+  console.log(input)
+  return (dispatch) => {
+    dispatch({ type: 'FETCHING_SUGGESTED_START_LOCATIONS' });
+
+    _fetchLocation(input).then(suggestedStartingLocations => dispatch({ type: 'DISPLAY_START_LOCATIONS', suggestedStartingLocations }));
+  };
 }
